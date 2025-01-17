14bb3b507c6a828dcd3a8e56a9d076fb82960e46
Arlind Halimi <arlindhalimi1604@gmail.com>
2025-01-17 14:11:50 +0100

    feat : crée class MotherEntity dan le fichier mother_entity.php

--- entities/mother_entity.php
+++ entities/mother_entity.php
@@ -3,3 +3,21 @@
 	* Classe d'un acteur
 	* @author Arlind Halimi
 	*/
+    class MotherEntity{
+        public function __construct(){}
+        
+          /**
+		* Récupération du id
+		* @return integer id de acteur
+		*/
+        public function getId(){
+            return $this->_id;
+        }
+        /**
+		* Mise à jour du id
+		* @param integer id
+		*/
+        public function setId(int $intId){
+            $this->_id = $intId;
+        }
+    }
