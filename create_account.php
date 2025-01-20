2ced58d1b08e7b7e0939f18b3f99222934839e34
DIELAINE Christophe <christophe.dielaine@ccicampus.fr>
2025-01-20 10:30:51 +0100

    feat : Mise a jour du create_account.php

--- create_account.php
+++ create_account.php
@@ -60,7 +60,6 @@ if (count($_POST) > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire d'inscription BingeWatchr</title>
-    <!-- Ajouter le CSS Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -136,7 +135,6 @@ if (count($_POST) > 0) {
         </form>
     </div>
 
-    <!-- Ajouter le JS de Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
