15c3e71467a3192db3b711baf0e0ae0825a5c025
Hugo Gomes <hugo.gomes-da-costa@ccicampus.fr>
2025-01-20 14:32:05 +0100

    ci : Récupération et intégration de la nav et du head

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-    <html lang="fr">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-            <title>BingeWatchr - Home</title>
-        </head>
-        <body>
-            <?php
+<?php
+include_once('head.php');
+
             require_once("entities/movie_entity.php");
 			require_once("models/movie_model.php");
 
@@ -48,8 +41,8 @@
                     ?>
                 </div>
             </div>
-            
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        </body>
-    </html>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
