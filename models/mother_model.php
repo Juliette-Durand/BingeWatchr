a35a94709535d60eea71ed5ad7a8093e3c5786e2
Hugo Gomes <hugo.gomes-da-costa@ccicampus.fr>
2025-01-20 14:41:39 +0100

    fix : Changement du nom de la BDD pour correspondre

--- models/mother_model.php
+++ models/mother_model.php
@@ -11,7 +11,7 @@
 			try{
 				// Connexion à la base de données
 				$this->_db = new PDO(
-								"mysql:host=localhost;dbname=bwr",  // Serveur et BDD
+								"mysql:host=localhost;dbname=bingewatchr",  // Serveur et BDD
 								"root",  		//Nom d'utilisateur de la base de données
 								"",	 	// Mot de passe de la base de données
 								array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC) // Mode de renvoi 
