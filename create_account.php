ffc3a3fff482f66a12dac475c3fd8ccf6c963946
DIELAINE Christophe <christophe.dielaine@ccicampus.fr>
2025-01-20 10:18:10 +0100

    feat : Mise a jour du create_account.php

--- create_account.php
+++ create_account.php
@@ -7,8 +7,6 @@ $strPassword = $_POST['password'] ?? "";
 $strBio = $_POST['bio'] ?? "";
 $strAvatar = $_FILES['avatar'] ?? "";
 
-var_dump($_POST);
-
 // Initialisation du tableau des erreurs
 $arrErrors = array();
 
@@ -53,66 +51,89 @@ if (count($_POST) > 0) {
 
 ?>
 
-<!-- Formulaire d'inscription -->
-<form name="formRegister" method="post" action="" enctype="multipart/form-data">
-    <legend>Formulaire d'inscription BingeWatchr</legend>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire d'inscription BingeWatchr</title>
+    <!-- Ajouter le CSS Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <!-- Formulaire d'inscription -->
+        <form name="formRegister" method="post" action="" enctype="multipart/form-data">
+            <legend>Formulaire d'inscription BingeWatchr</legend>
 
-    <!-- Affichage des erreurs -->
-    <?php if (isset($arrErrors['first_name'])): ?>
-        <div class="alert alert-danger" role="alert">
-            <?php echo $arrErrors['first_name']; ?>
-        </div>
-    <?php endif; ?>
+            <!-- Affichage des erreurs -->
+            <?php
+            if (isset($arrErrors['first_name'])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $arrErrors['first_name']; ?>
+                </div>
+            <?php } ?>
 
-    <label for="first_name">Prénom :</label><br>
-    <input type="text" id="first_name" name="first_name" class="form-control" value="<?php echo($strFirst_name); ?>"><br>
+            <label for="first_name">Prénom :</label><br>
+            <input type="text" id="first_name" name="first_name" class="form-control" value="<?php echo($strFirst_name); ?>"><br>
 
-    <?php if (isset($arrErrors['last_name'])): ?>
-        <div class="alert alert-danger" role="alert">
-            <?php echo $arrErrors['last_name']; ?>
-        </div>
-    <?php endif; ?>
+            <?php
+            if (isset($arrErrors['last_name'])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $arrErrors['last_name']; ?>
+                </div>
+            <?php } ?>
 
-    <label for="last_name">Nom :</label><br>
-    <input type="text" id="last_name" name="last_name" class="form-control" value="<?php echo($strLast_name); ?>"><br>
+            <label for="last_name">Nom :</label><br>
+            <input type="text" id="last_name" name="last_name" class="form-control" value="<?php echo($strLast_name); ?>"><br>
 
-    <?php if (isset($arrErrors['mail'])): ?>
-        <div class="alert alert-danger" role="alert">
-            <?php echo $arrErrors['mail']; ?>
-        </div>
-    <?php endif; ?>
+            <?php
+            if (isset($arrErrors['mail'])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $arrErrors['mail']; ?>
+                </div>
+            <?php } ?>
 
-    <label for="mail">E-mail :</label><br>
-    <input type="email" id="mail" name="mail" class="form-control" value="<?php echo($strMail); ?>"><br>
+            <label for="mail">E-mail :</label><br>
+            <input type="email" id="mail" name="mail" class="form-control" value="<?php echo($strMail); ?>"><br>
 
-    <?php if (isset($arrErrors['password'])): ?>
-        <div class="alert alert-danger" role="alert">
-            <?php echo $arrErrors['password']; ?>
-        </div>
-    <?php endif; ?>
+            <?php
+            if (isset($arrErrors['password'])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $arrErrors['password']; ?>
+                </div>
+            <?php } ?>
 
-    <label for="password">Mot de passe :</label><br>
-    <input type="password" id="password" name="password" class="form-control" value="<?php echo($strPassword); ?>"><br>
+            <label for="password">Mot de passe :</label><br>
+            <input type="password" id="password" name="password" class="form-control" value="<?php echo($strPassword); ?>"><br>
 
-    <?php if (isset($arrErrors['bio'])): ?>
-        <div class="alert alert-danger" role="alert">
-            <?php echo $arrErrors['bio']; ?>
-        </div>
-    <?php endif; ?>
+            <?php
+            if (isset($arrErrors['bio'])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $arrErrors['bio']; ?>
+                </div>
+            <?php } ?>
 
-    <label for="bio">Biographie :</label><br>
-    <textarea id="bio" name="bio" class="form-control"><?php echo($strBio); ?></textarea><br>
+            <label for="bio">Biographie :</label><br>
+            <textarea id="bio" name="bio" class="form-control"><?php echo($strBio); ?></textarea><br>
 
-    <?php if (isset($arrErrors['avatar'])): ?>
-        <div class="alert alert-danger" role="alert">
-            <?php echo $arrErrors['avatar']; ?>
-        </div>
-    <?php endif; ?>
+            <?php
+            if (isset($arrErrors['avatar'])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $arrErrors['avatar']; ?>
+                </div>
+            <?php } ?>
 
-    <label for="avatar">Avatar* :</label><br>
-    <input class="form-control" name="avatar" type="file"><br>
+            <label for="avatar">Avatar* :</label><br>
+            <input class="form-control" name="avatar" type="file"><br>
 
-    <p>
-        <input class="form-control btn btn-primary" type="submit" value="S'inscrire">
-    </p>
-</form>
+            <p>
+                <input class="form-control btn btn-primary" type="submit" value="S'inscrire">
+            </p>
+        </form>
+    </div>
+
+    <!-- Ajouter le JS de Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
