1441613e32df727a97da19a0e7c39e25e46bbc38
Juliette Durand <juliette.durand.pro4@gmail.com>
2025-01-22 14:22:30 +0100

    style : Mise à jour du style de la navbar

--- nav.php
+++ nav.php
@@ -6,7 +6,7 @@
 						<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 							<span class="navbar-toggler-icon"></span>
 						</button>
-					<div class="collapse navbar-collapse" id="navbarNavDropdown">
+					<div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
 						<ul class="navbar-nav d-flex align-items-center">
 							<li class="nav-item">
 								<a class="nav-link" aria-current="page" href="index.php">Accueil</a>
